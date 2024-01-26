824067175956b60c289286834086f1321c2f0d1f
Artur Karczmarczyk <artur@ideaspot.pl>
2024-01-26 14:30:47 +0100

    #5 easy admin for links management

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Link;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    public function __construct(
+        private readonly ParameterBagInterface $params,
+    )
+    {
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+//        return parent::index();
+
+        // Option 1. You can make your dashboard redirect to some common page of your backend
+        //
+         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+         return $this->redirect($adminUrlGenerator->setController(LinkCrudController::class)->generateUrl());
+
+        // Option 2. You can make your dashboard redirect to different pages depending on the user
+        //
+        // if ('jane' === $this->getUser()->getUsername()) {
+        //     return $this->redirect('...');
+        // }
+
+        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
+        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
+        //
+        // return $this->render('some/path/my-dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle($this->params->get('app.name'));
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Links', 'fas fa-sitemap', Link::class);
+    }
+}
---
+++ src/Controller/Admin/LinkCrudController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Link;
+use App\Enum\LinkStatusEnum;
+use App\Service\LinkService;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\ChoiceFilter;
+
+class LinkCrudController extends AbstractCrudController
+{
+    public function __construct(
+        private readonly LinkService $linkService,
+    )
+    {
+    }
+
+    public static function getEntityFqcn(): string
+    {
+        return Link::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('shortUrl')->hideOnForm()->formatValue(function ($value, $entity) {
+                return sprintf('<a href="%s/%s" target="_blank">%s <i class="fa fa-arrow-up-right-from-square"></i></a>', $this->getParameter('app.link_base_url'), $value, $value);
+            }),
+            UrlField::new('longUrl'),
+            TextField::new('shortUrl')->onlyOnForms()->setRequired(false),
+            BooleanField::new('status')->onlyWhenUpdating(),
+            DateTimeField::new('createdAt')->hideOnForm(),
+            TextField::new('shortUrl')->onlyOnDetail()->formatValue(function ($value, $entity) {
+                return sprintf('<img alt="qr code" style="max-width: 100%%" src="https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=%s" >', "{$this->getParameter('app.link_base_url')}/{$value}");
+            }),
+        ];
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('shortUrl')
+            ->add('longUrl')
+            ->add(ChoiceFilter::new('status')->setChoices([
+                'Active' => LinkStatusEnum::STATUS_ACTIVE->value,
+                'Inactive' => LinkStatusEnum::STATUS_INACTIVE->value,
+            ]))
+        ;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+        ;
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->linkService->shorten($entityInstance->getLongUrl(), $entityInstance->getShortUrl());
+    }
+}
