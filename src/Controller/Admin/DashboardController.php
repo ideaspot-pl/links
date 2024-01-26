8f1ff8ca86acdf7d97ec9fca701f4315be79608d
Artur Karczmarczyk <artur@ideaspot.pl>
2024-01-26 14:39:01 +0100

    #6 basic auth security

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,11 +5,15 @@ namespace App\Controller\Admin;
 use App\Entity\Link;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Security\Permission;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
+use function Symfony\Component\Translation\t;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -51,5 +55,23 @@ class DashboardController extends AbstractDashboardController
     {
         // yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Links', 'fas fa-sitemap', Link::class);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        $userMenuItems = [];
+
+        if ($this->isGranted(Permission::EA_EXIT_IMPERSONATION)) {
+            $userMenuItems[] = MenuItem::linkToExitImpersonation(t('user.exit_impersonation', domain: 'EasyAdminBundle'), 'fa-user-lock');
+        }
+
+        $userName = method_exists($user, '__toString') ? (string) $user : $user->getUserIdentifier();
+
+        return UserMenu::new()
+            ->displayUserName()
+            ->displayUserAvatar()
+            ->setName($userName)
+            ->setAvatarUrl(null)
+            ->setMenuItems($userMenuItems);
     }
 }
